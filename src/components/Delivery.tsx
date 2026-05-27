9596c1ad54618fcfe3b97754b16807566fe42bcc
regmisamyog15-dev <regmisamyog.15@gmail.com>
2026-05-27 12:16:21 +0545

    Update Delivery.tsx

--- src/components/Delivery.tsx
+++ src/components/Delivery.tsx
@@ -8,13 +8,13 @@ const platforms = [
   {
     name: "Foodmandu",
     url: "https://foodmandu.com/",
-    img: "/images/img34.webp"
+    img: "/images/img34.webp",
     description: "Order on the app & get it delivered fast",
   },
   {
     name: "Mero Kinamel",
     url: "https://www.merokinamel.com/",
-    img: "/images/img32.jpg"
+    img: "/images/img32.jpg" ,
     description: "Local delivery partner across Chitwan",
   },
 ];
